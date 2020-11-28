31be187d0915def9eb19c2363d6fd2fca2acf4f0
Timothy Carman <techtimcarman@outlook.com>
2020-11-28 06:43:21 -0800

    Project Setup

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 import { StaticQuery } from 'gatsby'
-
+import {Button} from 'antd'
+import 'antd/dist/antd.css'
 
 import '../style/index.scss'
 import ShoppingCart from "../assets/images/add_shopping_cart-black-18dp.svg"
@@ -34,12 +35,13 @@ const Layout = ({ children, site }) => (
                 {site.siteMetadata.siteName}
               </Link>
             </h1>
-            <div className="Header_Links">
-    <Link menuLinks={data.site.siteMetadata.menuLinks} siteTitle={data.site.siteMetadata.title} />
+            <div className="Header__Links">
+    <Link menuLinks={site.siteMetadata.menuLinks} />
             <div className="Header__summary snipcart-summary snipcart-checkout">
               <div className="Header__summary__title">
                 <img src={ShoppingCart} alt="Add To Shopping Cart" />
               </div>
+              
               <div className="Header__summary__line">
                 <span className="snipcart-total-items"></span>
               </div>
@@ -54,6 +56,7 @@ const Layout = ({ children, site }) => (
       <div className="Wrap" >
         <div className="Footer">
           Footer Love 
+          <Button type="primary">Primary</Button>
         </div>
       </div>
     </div>
